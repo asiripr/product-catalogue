29be4c4b66573bab94cc3a552c16fd93f6800df9
asiripr <asiripramodaya2000@gmail.com>
2024-08-30 07:00:20 +0530

    i build the project with relevant dependacies

---
+++ src/main/java/com/asiripr/product_catalogue/ProductCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.asiripr.product_catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asiripr/product_catalogue/ProductCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asiripr.product_catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
