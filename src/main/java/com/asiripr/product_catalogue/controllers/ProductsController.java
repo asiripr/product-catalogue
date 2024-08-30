a1a3525d266efd608804fbd70a1d654b81f91abe
asiripr <asiripramodaya2000@gmail.com>
2024-08-30 13:26:44 +0530

    create the products front end page

--- src/main/java/com/asiripr/product_catalogue/controllers/ProductsController.java
+++ src/main/java/com/asiripr/product_catalogue/controllers/ProductsController.java
@@ -1,8 +1,29 @@
 package com.asiripr.product_catalogue.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.asiripr.product_catalogue.models.MyProduct;
+import com.asiripr.product_catalogue.services.ProductRepository;
+
 
 @Controller
+@RequestMapping("/products")
 public class ProductsController {
 
+	
+	@Autowired
+	private ProductRepository repo;
+	
+	@GetMapping({"","/"})
+	public String showProductList(Model model) {
+		List<MyProduct> products = repo.findAll();
+		model.addAttribute("products", products);
+		return "prducts/index";
+	}
 }
--- src/main/java/com/asiripr/product_catalogue/services/ProductRepository.java
+++ src/main/java/com/asiripr/product_catalogue/services/ProductRepository.java
@@ -3,7 +3,7 @@ package com.asiripr.product_catalogue.services;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.asiripr.product_catalogue.models.MyProduct;
-
+// we can use this interface to read and write product from the db
 public interface ProductRepository extends JpaRepository<MyProduct, Integer>{
 
 }
