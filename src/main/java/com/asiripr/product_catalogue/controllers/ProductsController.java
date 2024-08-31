cf047989099f8ac76e8bb392f9d41304986b934c
asiripr <asiripramodaya2000@gmail.com>
2024-08-31 12:12:54 +0530

    improving functionalities of product page

--- src/main/java/com/asiripr/product_catalogue/controllers/ProductsController.java
+++ src/main/java/com/asiripr/product_catalogue/controllers/ProductsController.java
@@ -1,5 +1,8 @@
 package com.asiripr.product_catalogue.controllers;
 
+import java.io.InputStream;
+import java.nio.file.*;
+import java.sql.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.asiripr.product_catalogue.models.MyProduct;
@@ -74,5 +78,47 @@ public class ProductsController {
 			return "redirect:/products";
 		}
 		return "products/EditProduct";
+	}
+	
+	@PostMapping("/edit")
+	public String updateProduct(Model model, @RequestParam int id, @Valid @ModelAttribute MyProductDTO productDTO, BindingResult result) {
+		try {
+			MyProduct product = repo.findById(id).get();
+			model.addAttribute("product", product);
+			
+			if (result.hasErrors()) {
+				return "products/EditProducts";
+			}
+			
+			if (!productDTO.getImageFile().isEmpty()) {
+				//delete old image
+				String uploadDir = "public/images/";
+				Path oldImagePath = Paths.get(uploadDir + product.getImageFileName());
+			
+				try {
+					Files.delete(oldImagePath);
+				} catch (Exception e) {
+					System.out.println("Exception: "+e.getMessage());
+				}
+				
+				// save new image file
+				MultipartFile image = productDTO.getImageFile();
+				Date createdAt = new Date(id);
+				String storageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
+				
+				try (InputStream inputStream = image.getInputStream()){
+					Files.copy(inputStream, Paths.get(uploadDir + storageFileName),
+						StandardCopyOption.REPLACE_EXISTING);
+					}
+					product.setImageFileName(storageFileName);}
+				 
+			
+			
+			
+		} catch (Exception e) {
+			System.out.println("Exception: "+e.getMessage());
+		}
+		
+		return "redirect:/products";
 	}
 }
